fc51e1159c137390ff6324495fc41ae790e41d98
Johannes Aengenheyster <johannes.ae@hotmail.de>
2023-08-03 16:59:35 +0200

    add gendt_pmdb_excl based on rmonad and collapse, some time/memory leak tests

--- data_sources/pmdb/imp_pmdb.R
+++ data_sources/pmdb/imp_pmdb.R
@@ -61,6 +61,8 @@ gendt_pmdb_excl <- function(PMDB_FILE, only_pms=T) {
 
     ## check that all countries with country string convert to country code
     if (dt_pmdb_excl2[is.na(iso3c) & country != "", .N] > 0) {stop("not all countries are matched")}
+    
+    
 
     ## test_file("~/Dropbox/phd/pmdata/data_sources/pmdb/tests_gendt_pmdb_excl.R")
 
@@ -73,30 +75,132 @@ gendt_pmdb_excl <- function(PMDB_FILE, only_pms=T) {
         
 }
 
-test_gendt_pmdb_excl <- function(dt_pmdb_excl2) {
-    test_that("true is true", {expect_true(T)})
-    test_that("all_PMs have ID", {expect_true(dt_pmdb_excl2[, none(ID, is.na)])})
-
+gendtm_pmdb_excl <- function(PMDB_FILE, only_pms=T) {
+    if (as.character(match.call()[[1]]) %in% fstd){browser()}
+    1;1;1;1;1;1;1;1;1;1;1;1;1;1;1;1;1;1;1;1;1;1;1;1;1;1;1;
+    #' just read in the excel file, add a few auxiliary columns
+    #' use rmonad (leaks memory tho)
     
+    basic_cols <- as_monad(list(
+        country = c("Country where museum is located", "Museum_country"),
+        name = c("Museum_name", "Name of museum"),
+        year_opened_str =  c("Opening year", "Museum_opening year"),
+        year_closed_str = c("Closing year", "Museum_closing year"),
+        museum_status = c("Museum status", "Museum_status"))) %>% tag("basic_cols")
+
+    ## dt_input <- fread(PMDB_FILE)[2:5] %>>% as_monad %>% tag("input") 
+
+    m_excl_prep <- PMDB_FILE %>>% (\(x) fread(x)[2:.N]) %>% tag("dt_input") %>%
+        funnel(basic_cols) %>%
+        tag2((\(x) if (any(names(x[[2]]) %in% names(x[[1]])))
+                   {stop("new cols already there")}), "check_basic_cols")  %*>%
+        (\(dtx, bcols) dtx[, .SD, .SDcols = unlist(map(bcols, ~vrbl_fndr(dtx, .x)))] %>%
+                       setnames(new = names(bcols))) %>% tag("dt_wbcol") %>% 
+        funnel(view(., "dt_input")) %*>% cbind %>>%
+            d(, `:=`(ID = as.integer(ID), # edit ID columns 
+                     iso3c = countrycode(country, "country.name", "iso3c"), # add iso3c column
+                     year_opened = as.integer(year_opened_str),
+                     year_closed = as.integer(year_closed_str))) %>% tag("dt_excl")
+
+    ## plot(m_excl_prep, label = \(x) get_tag(x))
+    ## m_excl_prep %>% missues
+
+    ## run tests
+    m_excl <- m_excl_prep %>%
+        tag2((\(x) if (x[is.na(iso3c) & country != "", .N] > 0)
+                       {stop("not all countries matched")}), "check_countrycode_match") %>%
+        tag2((\(x) if (x[is.na(ID), .N] > 0) {stop("not all IDs there")}), "check_IDs_presence") %>%
+        tag2((\(x) if (x[, .N, ID][, max(N)] > 1) {stop("not all IDs are unique")}), "check_ID_unqs")
+        
+    return(m_excl)
+
+    ## mx <- 10 %>% as_monad %>>% sqrt %>% tag("s1") %>>% sqrt %>% tag("s2")
+    ## plot(mx)
+
+    ## funnel(view(mx, "s1"), view(mx, "s2")) %>% tag2(len, "len") %>% get_value(tag ="len")
+
+    ## more complicated operation
+    ## funnel(view(mx, "s1"), view(mx, "s2")) %*>% tag2((\(x,y) x+y), "sum")
+    
+    ## 
+    ## funnel(view(mx, "s1"), view(mx, "s2")) %>% tag2((\(x) x[[1]] + x[[2]]), "sum") %*>%
+    ##     (\(x,y) x * y) %>% plot(label = "code")
+
+    ## funnel(view(mx, "s1"), view(mx, "s2")) %>% 
+
 }
 
-test_gendt_pmdb_excl(dtx)
+## gendtm_pmdb_excl(PMDB_FILE)
+
+gendtc_pmdb_excl <- function(PMDB_FILE, only_pms = T) {
+    if (as.character(match.call()[[1]]) %in% fstd){browser()}
+    1;1;1;1;1;1;1;1;1;1;1;1;1;1;1;1;1;1;1;1;1;1;1;1;1;1;1;
+
+    dt_pmdb_excl <- fread(PMDB_FILE)[2:.N]# skip second column
+    ## dt_pmdb_excl[, map(.SD, class)] %>% melt(id.vars = "ID") %>% .[value != "character"] %>% .[, .N, value]
+    ## dtx <- read.xlsx2(PMDB_FILE, sheetName = "Blad1") %>% adt() %>% .[2:.N]
+    ## dtx[653:.N] %>% melt(id.vars = "ID") %>% .[value != ""]
+    ## some additional basic aux columns, no transformation
+
+    basic_cols <- list(
+        country = c("Country where museum is located", "Museum_country"),
+        name = c("Museum_name", "Name of museum"),
+        year_opened_str =  c("Opening year", "Museum_opening year"),
+        year_closed_str = c("Closing year", "Museum_closing year"),
+        museum_status = c("Museum status", "Museum_status")) %>%
+        map(., ~vrbl_fndr(dt_pmdb_excl, .x)) %>% unlist %>%
+        setNames(names(.), .) # change name and value for collapse
+
+    if (any(basic_cols %in% names(dt_pmdb_excl))) {stop("new cols already there")}
+
+    dt_pmdb_excl2 <- frename(dt_pmdb_excl, basic_cols, cols = names(basic_cols)) %>%
+        ftransform(ID = as.integer(ID),
+            iso3c = countrycode(country, "country.name", "iso3c"),
+            year_opened = as.integer(year_opened_str),
+            year_closed = as.integer(year_closed_str))
 
 
-test_file("~/Dropbox/phd/pmdata/data_sources/pmdb/tests_gendt_pmdb_excl.R")
+    if (fsubset(dt_pmdb_excl2, is.na(iso3c) & country != "") %>% fnrow > 0) {stop("not all countries are matched")}
 
 
-dtx[, none(ID, is.na)]
+    return(dt_pmdb_excl2)
 
 
-dt_pmdb_excl <- gendt_pmdb_excl(PMDB_FILE, only_pms = F)
+}
 
 
 
-1+1
+gendtc_pmdb_excl(PMDB_FILE)
+debug("gendtc_pmdb_excl")
 
-path <- testthat_example("failure")
-test_file(path)
+
+
+## compare dts: 
+dt_pmdb_excl_dt <- gendt_pmdb_excl(PMDB_FILE)[, `:=`(`Museum_opening year` = NULL, Museum_name = NULL,
+                       Museum_country = NULL, `Museum_closing year` = NULL,
+                       Museum_status = NULL)]
+
+dt_pmdb_excl_clps <- gendtc_pmdb_excl(PMDB_FILE) %>% .[, .SD, .SDcols = names(dt_pmdb_excl_dt)]
+
+identical(dt_pmdb_excl_dt, dt_pmdb_excl_clps)
+fsetdiff(dt_pmdb_excl_dt, dt_pmdb_excl_clps)
+
+
+
+    
+
+
+
+microbenchmark(
+    dt = gendt_pmdb_excl(PMDB_FILE),
+    clps = gendtc_pmdb_excl(PMDB_FILE), times = 5)
+
+## dtx[, none(ID, is.na)]
+
+## dt_pmdb_excl <- gendt_pmdb_excl(PMDB_FILE, only_pms = F)
+
+
+
 
 PMDATA_DIR <- "/home/johannes/Dropbox/phd/pmdata/"
 PMDB_DIR <- paste0(PMDATA_DIR, "data_sources/pmdb/")
@@ -147,21 +251,39 @@ mem_change(dtx <- gendt_pmdb_excl(PMDB_FILE, only_pms = F))
 object_size(dtx)
 
 
+
 for(i in 1:40) {
-    dtx <- gendt_pmdb_excl(PMDB_FILE, only_pms = F)
+    m1 <- as.integer(pryr::mem_used())
+    ## dtx <- gendt_pmdb_excl(PMDB_FILE, only_pms = F)
+    ## dtx <- gendtm_pmdb_excl(PMDB_FILE, only_pms = F)
+    dtx <- 10 %>>% sqrt
     rm(dtx)    
     gc()
-    print(pryr::mem_used())
+    m2 <- as.integer(pryr::mem_used())
+    print(m2-m1)
 }
 
 
 
 
 
+## ** testthat-based tests
+## test_gendt_pmdb_excl <- function(dt_pmdb_excl2) {
+##     test_that("true is true", {expect_true(T)})
+##     test_that("all_PMs have ID", {expect_true(dt_pmdb_excl2[, none(ID, is.na)])})
+
+    
+## }
+
+## test_gendt_pmdb_excl(dtx)
+
+
+## test_file("~/Dropbox/phd/pmdata/data_sources/pmdb/tests_gendt_pmdb_excl.R")
+
+## path <- testthat_example("failure")
+## test_file(path)
 
     
     
     
-
-
-
+    
