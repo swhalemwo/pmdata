e2627cb055f94ed65b64ee3c55c752a417f85f06
Johannes Aengenheyster <johannes.ae@hotmail.de>
2023-09-04 11:50:28 +0200

    uncomment dt based gendt_pmdb_excl, move rmonad-based function to explore/rmonad/monad_test3.R, use new naming scheme

--- R/imp_pmdb.R
+++ R/imp_pmdb.R
@@ -26,112 +26,56 @@ vrbl_fndr <- function(df, penl_vrbls) {
 
 
 
-#' generates the most basic data.frame of the google sheet
-#' (only handful of columns standardized)
-#'
-#' @param PMBD_FILE path to CSV download from google sheets
-#' @param only_pms whether to include only private museums that are currently open
-#' @export
-gendt_pmdb_excl <- function(PMDB_FILE = DATA_LOCS$PMDB_FILE, only_pms=T) {
-    if (as.character(match.call()[[1]]) %in% fstd){browser()}
+## #' generates the most basic data.frame of the google sheet
+## #' (only handful of columns standardized)
+## #'
+## #' @param PMBD_FILE path to CSV download from google sheets
+## #' @param only_pms whether to include only private museums that are currently open
+## #' @export
+## gendtb_pmdb_excl <- function(PMDB_FILE = DATA_LOCS$PMDB_FILE, only_pms=T) {
+##     if (as.character(match.call()[[1]]) %in% fstd){browser()}
     
-    dt_pmdb_excl <- fread( PMDB_FILE)[2:.N]# skip second column
-    ## dt_pmdb_excl[, map(.SD, class)] %>% melt(id.vars = "ID") %>% .[value != "character"] %>% .[, .N, value]
-    ## dtx <- read.xlsx2(PMDB_FILE, sheetName = "Blad1") %>% adt() %>% .[2:.N]
-    ## dtx[653:.N] %>% melt(id.vars = "ID") %>% .[value != ""]
-    ## some additional basic aux columns, no transformation
-    basic_cols <- list(
-        country = c("Country where museum is located", "Museum_country"),
-        name = c("Museum_name", "Name of museum"),
-        year_opened_str =  c("Opening year", "Museum_opening year"),
-        year_closed_str = c("Closing year", "Museum_closing year"),
-        museum_status = c("Museum status", "Museum_status"))
-    ## throw error if any of the aux columns are already there
-    if (len(setdiff(names(basic_cols), names(dt_pmdb_excl))) != len(names(basic_cols))) {
-        stop("new cols already there")}
-    ## select basic_cols subset of columns, rename it
-    dt_pmdb_excl2 <- dt_pmdb_excl[, .SD, .SDcols = unlist(map(basic_cols, ~vrbl_fndr(dt_pmdb_excl, .x)))] %>% 
-        setnames(new = names(basic_cols))%>% # update names 
-        cbind(dt_pmdb_excl) %>% # combine with old data
-        .[, `:=`(ID = as.integer(ID), # edit ID columns 
-                 iso3c = countrycode(country, "country.name", "iso3c"), # add iso3c column
-                 year_opened = as.integer(year_opened_str),
-                 year_closed = as.integer(year_closed_str))]
+##     dt_pmdb_excl <- fread( PMDB_FILE)[2:.N]# skip second column
+##     ## dt_pmdb_excl[, map(.SD, class)] %>% melt(id.vars = "ID") %>% .[value != "character"] %>% .[, .N, value]
+##     ## dtx <- read.xlsx2(PMDB_FILE, sheetName = "Blad1") %>% adt() %>% .[2:.N]
+##     ## dtx[653:.N] %>% melt(id.vars = "ID") %>% .[value != ""]
+##     ## some additional basic aux columns, no transformation
+##     basic_cols <- list(
+##         country = c("Country where museum is located", "Museum_country"),
+##         name = c("Museum_name", "Name of museum"),
+##         year_opened_str =  c("Opening year", "Museum_opening year"),
+##         year_closed_str = c("Closing year", "Museum_closing year"),
+##         museum_status = c("Museum status", "Museum_status"))
+##     ## throw error if any of the aux columns are already there
+##     if (len(setdiff(names(basic_cols), names(dt_pmdb_excl))) != len(names(basic_cols))) {
+##         stop("new cols already there")}
+##     ## select basic_cols subset of columns, rename it
+##     dt_pmdb_excl2 <- dt_pmdb_excl[, .SD, .SDcols = unlist(map(basic_cols, ~vrbl_fndr(dt_pmdb_excl, .x)))] %>% 
+##         setnames(new = names(basic_cols))%>% # update names 
+##         cbind(dt_pmdb_excl) %>% # combine with old data
+##         .[, `:=`(ID = as.integer(ID), # edit ID columns 
+##                  iso3c = countrycode(country, "country.name", "iso3c"), # add iso3c column
+##                  year_opened = as.integer(year_opened_str),
+##                  year_closed = as.integer(year_closed_str))]
 
-    ## check that all countries with country string convert to country code
-    if (dt_pmdb_excl2[is.na(iso3c) & country != "", .N] > 0) {stop("not all countries are matched")}
+##     ## check that all countries with country string convert to country code
+##     if (dt_pmdb_excl2[is.na(iso3c) & country != "", .N] > 0) {stop("not all countries are matched")}
     
     
 
-    ## test_file("~/Dropbox/phd/pmdata/data_sources/pmdb/tests_gendt_pmdb_excl.R")
+##     ## test_file("~/Dropbox/phd/pmdata/data_sources/pmdb/tests_gendt_pmdb_excl.R")
 
-    ## test_that("all PMs have ID", {expect_true(dtx[is.na(ID), .N] == 0)})
-    ## test_that("all_PMs have ID", {expect_true(dtx[, none(ID, is.na)])})
-    ## if (dt_pmdb_excl2[is.na(ID), .N > 0]) {stop("not all PMs have ID")}
-    ## if (dt_pmdb_excl2[, uniqueN(ID) == .N]) {stop("not all IDs are unique")}
+##     ## test_that("all PMs have ID", {expect_true(dtx[is.na(ID), .N] == 0)})
+##     ## test_that("all_PMs have ID", {expect_true(dtx[, none(ID, is.na)])})
+##     ## if (dt_pmdb_excl2[is.na(ID), .N > 0]) {stop("not all PMs have ID")}
+##     ## if (dt_pmdb_excl2[, uniqueN(ID) == .N]) {stop("not all IDs are unique")}
     
-    return(dt_pmdb_excl2)
+##     return(dt_pmdb_excl2)
         
-}
+## }
 
 
-#' generates the most basic data.frame of the google sheet as rmonad (only handful of columns standardized)
-#'
-#' @param PMBD_FILE path to CSV download from google sheets
-#' @param only_pms whether to include only private museums that are currently open
-#' @export
-gendtm_pmdb_excl <- function(PMDB_FILE = DATA_LOCS$PMDB_FILE, only_pms=T) {
-    if (as.character(match.call()[[1]]) %in% fstd){browser()}
-    1;1;1;1;1;1;1;1;1;1;1;1;1;1;1;1;1;1;1;1;1;1;1;1;1;1;1;
-        
-    basic_cols <- as_monad(list(
-        country = c("Country where museum is located", "Museum_country"),
-        name = c("Museum_name", "Name of museum"),
-        year_opened_str =  c("Opening year", "Museum_opening year"),
-        year_closed_str = c("Closing year", "Museum_closing year"),
-        museum_status = c("Museum status", "Museum_status"))) %>% tag("basic_cols")
 
-    ## dt_input <- fread(PMDB_FILE)[2:5] %>>% as_monad %>% tag("input") 
-
-    m_excl_prep <- PMDB_FILE %>>% (\(x) fread(x)[2:.N]) %>% tag("dt_input") %>%
-        funnel(basic_cols) %>%
-        tag2((\(x) if (any(names(x[[2]]) %in% names(x[[1]])))
-                   {stop("new cols already there")}), "check_basic_cols")  %*>%
-        (\(dtx, bcols) dtx[, .SD, .SDcols = unlist(map(bcols, ~vrbl_fndr(dtx, .x)))] %>%
-                       setnames(new = names(bcols))) %>% tag("dt_wbcol") %>% 
-        funnel(view(., "dt_input")) %*>% cbind %>>%
-            d(, `:=`(ID = as.integer(ID), # edit ID columns 
-                     iso3c = countrycode(country, "country.name", "iso3c"), # add iso3c column
-                     year_opened = as.integer(year_opened_str),
-                     year_closed = as.integer(year_closed_str))) %>% tag("dt_excl")
-
-    ## plot(m_excl_prep, label = \(x) get_tag(x))
-    ## m_excl_prep %>% missues
-
-    ## run tests
-    m_excl <- m_excl_prep %>%
-        tag2((\(x) if (x[is.na(iso3c) & country != "", .N] > 0)
-                       {stop("not all countries matched")}), "check_countrycode_match") %>%
-        tag2((\(x) if (x[is.na(ID), .N] > 0) {stop("not all IDs there")}), "check_IDs_presence") %>%
-        tag2((\(x) if (x[, .N, ID][, max(N)] > 1) {stop("not all IDs are unique")}), "check_ID_unqs")
-        
-    return(m_excl)
-
-    ## mx <- 10 %>% as_monad %>>% sqrt %>% tag("s1") %>>% sqrt %>% tag("s2")
-    ## plot(mx)
-
-    ## funnel(view(mx, "s1"), view(mx, "s2")) %>% tag2(len, "len") %>% get_value(tag ="len")
-
-    ## more complicated operation
-    ## funnel(view(mx, "s1"), view(mx, "s2")) %*>% tag2((\(x,y) x+y), "sum")
-    
-    ## 
-    ## funnel(view(mx, "s1"), view(mx, "s2")) %>% tag2((\(x) x[[1]] + x[[2]]), "sum") %*>%
-    ##     (\(x,y) x * y) %>% plot(label = "code")
-
-    ## funnel(view(mx, "s1"), view(mx, "s2")) %>% 
-
-}
 
 
 #' generates basic data.frame of the google sheet (using collapse)
@@ -140,8 +84,8 @@ gendtm_pmdb_excl <- function(PMDB_FILE = DATA_LOCS$PMDB_FILE, only_pms=T) {
 #' @param PMBD_FILE path to CSV download from google sheets
 #' @param only_pms whether to include only private museums that are currently open
 #' @export
-gendtc_pmdb_excl <- function(PMDB_FILE = DATA_LOCS$PMDB_FILE, only_pms = T) {
-    if (as.character(match.call()[[1]]) %in% fstd){browser()}
+gd_pmdb_excl <- function(PMDB_FILE = DATA_LOCS$PMDB_FILE, only_pms = T) {
+    ## if (as.character(match.call()[[1]]) %in% fstd){browser()}
 
     dt_pmdb_excl <- fread(PMDB_FILE) %>% adt %>% .[2:.N] # skip second column
     ## dt_pmdb_excl[, map(.SD, class)] %>% melt(id.vars = "ID") %>% .[value != "character"] %>% .[, .N, value]
@@ -174,22 +118,8 @@ gendtc_pmdb_excl <- function(PMDB_FILE = DATA_LOCS$PMDB_FILE, only_pms = T) {
     } else if (!only_pms) {
         return(dt_pmdb_excl2)
     }
-    
 
 }
 
 
 
-
-## #' test function
-## #'
-## #' this is some description
-## #' @export 
-## testf <- function() {
-##     15
-## }
-
-## #' @export 
-## testf2 <- function() {
-##     290
-## }
