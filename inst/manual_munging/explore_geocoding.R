25428531f9852525c1b70dc7593cb05fb8bad457
swhalemwo <jojo.ae@hotmail.de>
2024-02-26 17:30:30 +0100

    more geocoding exploration, google API looks not bad

--- inst/manual_munging/explore_geocoding.R
+++ inst/manual_munging/explore_geocoding.R
@@ -1,3 +1,5 @@
+## * geocoding fun  
+
 library(pmdata)
 library(countrycode)
 library(collapse)
@@ -5,12 +7,16 @@ library(purrr)
 
 options(width = 115)
 
+
+
 PMDATA_LOCS <- gc_pmdata_locs()
 
 dt_pmdb_prep <- gd_pmdb_excl(only_pms = F) %>% gd_pmdb
 
 dt_pmdb <- dt_pmdb_prep[, .(ID, name, museum_status, iso3c, city)] %>%
     .[museum_status %in% c("private museum", "closed")]
+
+## ** using existing datasets
 
 ## read some more data 
 
@@ -54,4 +60,60 @@ dt_match_geonames <- join(dt_unmatch_worldcities,  dt_geonames, on = .c(iso3c, c
     
 
 
-## ** MOW FUN
+## ** via API
+
+library(tidygeocoder)
+
+## geo-code test: once with name, once just city
+dt_pmdb_gct <- dt_pmdb[1:5] %>%
+    .[, `:=`(addr_name = sprintf("%s, %s, %s", name, city, countrycode(iso3c, "iso3c", "country.name")),
+             addr_city = sprintf("%s, %s", city, countrycode(iso3c, "iso3c", "country.name")))]
+
+
+dt_pmdb_gct_name <- geocode(atb(dt_pmdb_gct), addr_name)
+dt_pmdb_gct_city <- geocode(atb(dt_pmdb_gct), addr_city)
+
+## seems to be possible to just pass name and get different coords
+adt(dt_pmdb_gct_name)[, lat]
+adt(dt_pmdb_gct_city)[, lat]
+
+## not sure how certain that model is..
+dt_pmdb_gct_dtls <- geocode(atb(dt_pmdb_gct), addr_name, limit =5, return_input = F)
+
+dt_pmdb_gct_dtls <- geocode(atb(dt_pmdb_gct), addr_name, limit =5, full_results = T, return_input = F)
+
+
+## work around nonsense limit restrictions with DT
+geocode(tibble(addr = "Albert Heijn, amsterdam, the netherlands"), addr, limit = 5, return_input = F)
+
+dt_markets <- data.table(addr = c("Albert Heijn, amsterdam, the netherlands",
+                                  "Jumbo, Amsterdam, the netherlands"), id = c(1,2)) %>%
+    .[, addr2 := addr] %>% atb
+    
+
+dt_markets_gcdd <- geocode(dt_markets, addr, limit = 5, return_input = F)
+
+
+dt_markets %>% adt %>% .[, geocode(atb(.SD), addr, limit = 5, return_input = F), by = id]
+
+
+
+## try OSM for closed ones
+dt_pmdb_gctc <- dt_pmdb[museum_status == "closed", .SD[1:5]] %>%
+    .[, addr_name := sprintf("%s, %s, %s", name, city, countrycode(iso3c, "iso3c", "country.name"))]
+
+## geo code test closed coded
+dt_pmdb_gctcd <- geocode(atb(dt_pmdb_gctc), addr_name)
+                           
+
+## geocode argics
+geocode(atb(dt_pmdb_gctc), addr_name, method = "arcgis", limit = 5, return_input = F)
+
+
+## google api
+
+dt_google_full <- geocode(atb(dt_pmdb_gctc), addr_name, method = "google", limit = 5, return_input = F, full_result = T)
+
+dt_markets_gcdd <- geocode(dt_markets, method = "google", addr, limit = 5, return_input = F)
+
+adt(dt_google_full)[4] %>% adf
