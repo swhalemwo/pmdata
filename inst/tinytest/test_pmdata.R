05a1c113f8af28fdeaf56c3c5e804702f298c624
swhalemwo <jojo.ae@hotmail.de>
2025-02-10 16:25:49 +0100

    uncomment expensive artprice checks

--- inst/tinytest/test_pmdata.R
+++ inst/tinytest/test_pmdata.R
@@ -235,10 +235,10 @@ expect_true(t_af_coverage(), info = "test Artfacts coverage")
 
 ## ** ---------------- artprice checks --------------
 
-
-expect_true(pmdata:::t_gwd_ap_unqcheck(
-                         FILE_AP_ARTIST_YEAR = PMDATA_LOCS$FILE_AP_ARTIST_YEAR,
-                         FILE_AP_ARTIST_ID = PMDATA_LOCS$FILE_AP_ARTIST_ID,
-                         FILE_AP_UNQCHECK = PMDATA_LOCS$FILE_AP_UNQCHECK),
-            info = paste0(c("check that all suspicious (similar) sounding names have been checked",
-                          " whether they are actually the same person")))
+## this takes like 30secs to run.. 
+## expect_true(pmdata:::t_gwd_ap_unqcheck(
+##                          FILE_AP_ARTIST_YEAR = PMDATA_LOCS$FILE_AP_ARTIST_YEAR,
+##                          FILE_AP_ARTIST_ID = PMDATA_LOCS$FILE_AP_ARTIST_ID,
+##                          FILE_AP_UNQCHECK = PMDATA_LOCS$FILE_AP_UNQCHECK),
+##             info = paste0(c("check that all suspicious (similar) sounding names have been checked",
+##                           " whether they are actually the same person")))
