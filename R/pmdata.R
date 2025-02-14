4be851fbe94e00279a753639f739e2cb3162a338
swhalemwo <jojo.ae@hotmail.de>
2025-02-14 18:50:15 +0100

    more memoise testing: now in own file

--- R/pmdata.R
+++ R/pmdata.R
@@ -103,5 +103,3 @@ globalVariables(c("PMDATA_LOCS", ".","%>%", "fstd", "adt"))
 ## PMDATA_LOCS <- gc_pmdata_locs()
 
 
-## gwd_ap_ls_match <- memoise(f = gwd_ap_ls_match, cache = gc_cache_pmdata())
-gd_duckdb_sim_ap_ls <- memoise(f = gd_duckdb_sim_ap_ls, cache = gc_cache_pmdata())
---
+++ R/to_memoise.R
@@ -0,0 +1,2 @@
+## gwd_ap_ls_match <- memoise(f = gwd_ap_ls_match, cache = gc_cache_pmdata())
+gd_duckdb_sim_ap_ls <- memoise(f = gd_duckdb_sim_ap_ls, cache = gc_cache_pmdata())
