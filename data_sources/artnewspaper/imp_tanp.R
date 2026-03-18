38a4e32f5ff914cd4869dae7a1472455bade4e72
swhalemwo <jojo.ae@hotmail.de>
2026-03-18 12:15:18 +0100

    add tanp 08 (only 63)

--- data_sources/artnewspaper/imp_tanp.R
+++ data_sources/artnewspaper/imp_tanp.R
@@ -22,6 +22,10 @@ gd_tanp_cbn <- function() {
     ## more systematic approach
     ## read in data
 
+    dt_tanp_08 <- fread("/home/johannes/Dropbox/phd/pmdata/data_sources/artnewspaper/tanp_08.csv") %>%
+        .[, map(.SD, trimws)] %>%
+        .[, `:=`(id  = paste0("tanp08_", 1:.N), city = stringr::str_to_title(trimws(city)))]
+
     dt_tanp_09 <- fread("/home/johannes/Dropbox/phd/pmdata/data_sources/artnewspaper/tanp_09.csv") %>%
         .[, map(.SD, trimws)] %>%
         .[, `:=`(id  = paste0("tanp09_", 1:.N), city = stringr::str_to_title(trimws(city)))]
@@ -101,7 +105,7 @@ gd_tanp_cbn <- function() {
 
     ## combine
     dt_tanp_cbn <- map(list(
-        dt_tanp_09, dt_tanp_10, dt_tanp_11,
+        dt_tanp_08, dt_tanp_09, dt_tanp_10, dt_tanp_11,
         dt_tanp_12, dt_tanp_13, dt_tanp_14, dt_tanp_15, dt_tanp_16, dt_tanp_17, dt_tanp_18,
         dt_tanp_19, dt_tanp_20, dt_tanp_21, dt_tanp_22, dt_tanp_23, dt_tanp_24),
                        ~.x[, .(id, museum, city, total)]) %>%
